3458e1f8d378d9cebc0146208198cf667b79d3f7
luuk <lvan-gef@student.codam.nl>
2024-05-13 22:19:10 +0200

    Fixe echo problem, and now can be used with multie buffers

--- lua/norminette-check/init.lua
+++ lua/norminette-check/init.lua
@@ -4,6 +4,7 @@ if vim.v.shell_error ~= 0 then
 	return
 end
 
+local utils = require("norminette-check.utils")
 local M = {}
 
 -- set global group and sign
@@ -12,13 +13,13 @@ local norm_check_error = "CodamNormCheckError"
 vim.fn.sign_define(norm_check_error, {text = ">>", texthl = "Error", linehl = "", numhl = ""})
 
 local getErrors = function(path)
-	local norminette_output = vim.fn.system("norminette " .. vim.fn.shellescape(path))
+	local output = vim.fn.system("norminette " .. vim.fn.shellescape(path))
     if vim.v.shell_error == 0 then
 		return ""
 	end
 
-	local command = "echo " .. vim.fn.shellescape(norminette_output) .. " | grep -E '^(Error|Notice)' | uniq | sed -n 's/.*line: *\\([0-9]*\\), col: *\\([0-9]*\\).*:\\t\\(.*\\)/\\1:\\2:\\3/p'"
-	local output = vim.fn.system(command)
+	local command = "echo " .. vim.fn.shellescape(output) .. " | grep -E '^(Error|Notice)' | uniq | sed -n 's/.*line: *\\([0-9]*\\), col: *\\([0-9]*\\).*:\\t\\(.*\\)/\\1:\\2:\\3/p'"
+	output = vim.fn.system(command)
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({{"Something went wrong while parsing the output...", "ErrorMsg"}}, true, {})
 		return nil
@@ -26,6 +27,7 @@ local getErrors = function(path)
 
 	return output
 end
+
 
 local errors = {}
 
@@ -45,18 +47,18 @@ M.NormCheck = function ()
 		return
 	end
 
-	-- add errors to the table
+	local tmp_errors = {}
 	for line in result:gmatch("[^\r\n]+") do
 		local lnum, col, msg = line:match("(%d+):(%d+):(.+)")
 		if lnum and col and msg then
-			table.insert(errors, {lnum = tonumber(lnum), col = tonumber(col), text = msg})
+			table.insert(tmp_errors, {lnum = tonumber(lnum), col = tonumber(col), text = msg, buffnr = buffnr})
 		end
 	end
+	errors[buffnr] = utils.uniqTables(tmp_errors, errors[buffnr])
 
-	-- clear prev state and insert new state
 	vim.fn.sign_unplace(norm_check_group)
-	for _, err in ipairs(errors) do
-		vim.fn.sign_place(0, norm_check_group, norm_check_error, buffnr, {
+	for _, err in ipairs(errors[buffnr]) do
+		vim.fn.sign_place(0, norm_check_group, norm_check_error, err.buffnr, {
 			lnum = err.lnum,
 			priority = 100
 		})
@@ -70,11 +72,17 @@ end
 
 -- echo the message backout
 M.NormShow = function ()
-	local cursor_pos = vim.api.nvim_win_get_cursor(0)
+	local buffnr = vim.api.nvim_get_current_buf()
+	local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
-	for _, err in ipairs(errors) do
-		if err[1] == cursor_pos then
-			vim.api.nvim_echo({{err[3], "InfoMsg"}}, true, {})
+	if errors[buffnr] == nil then
+		return
+	end
+
+	for _, err in ipairs(errors[buffnr]) do
+		if err.lnum == row and err.buffnr == buffnr then
+			vim.api.nvim_echo({{err.text, "InfoMsg"}}, true, {})
+			break
 		end
 	end
 end
