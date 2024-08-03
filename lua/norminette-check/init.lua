b0309cfc93d0779a138759f058949b1bed13d858
luuk <lvan-gef@student.codam.nl>
2024-08-03 04:47:40 +0200

    rename and reformat code

--- lua/norminette-check/init.lua
+++ lua/norminette-check/init.lua
@@ -1,11 +1,11 @@
 local qf = require("norminette-check.qf_helpers")
 local plug_id = "norminette"
-local M = {}
+local normi = {}
 
----Run's norminette and return the errors
----@param path string
+---Run's norminette and parse norminette
+---@param path string  Path to the file you want to check
 ---@return table | nil
-local parseNorminette = function(path)
+local parseNormi = function(path)
 	local output = vim.fn.system("norminette " .. vim.fn.shellescape(path) .. " 2>&1")
 	local status = vim.v.shell_error
 
@@ -56,44 +56,43 @@ local parseNorminette = function(path)
 	end
 
 	if status == 0 and #errors ~= 0 then
-		vim.api.nvim_echo({ { "Norminette exit code: " .. status .. ".\nBut we have " .. #errors .. "error's", "ErrorMsg" } }, true, {})
+		vim.api.nvim_echo(
+		{ { "Norminette exit code: " .. status .. ".\nBut we have " .. #errors .. "error's", "ErrorMsg" } }, true, {})
 		return nil
 	end
 
 	return errors
 end
 
----run's norminette and add errors to the quickfix list
-M.NormCheck = function()
-	local buffnr = vim.api.nvim_get_current_buf()
-	local path = vim.api.nvim_buf_get_name(buffnr)
+---Check for norminette errors in the current buffer
+normi.NormiCheck = function()
+	local path = vim.api.nvim_buf_get_name(0)
 	if path == "" then
 		return
 	end
-	local name = vim.fn.fnamemodify(path, ":t:r")
 
+	local name = vim.fn.fnamemodify(path, ":t:r")
 	if name == nil then
 		return
 	end
 
-	local errors = parseNorminette(path)
+	local errors = parseNormi(path)
 	if errors == nil then
-		M.NormClear()
+		normi.NormiClear()
 		return
 	end
 
 	qf.append_errors(errors, plug_id, name)
 end
 
----clear errors from the qf-list given the filename
-M.NormClear = function()
-	local buffnr = vim.api.nvim_get_current_buf()
-	local path = vim.api.nvim_buf_get_name(buffnr)
+---clear norminette errors from the qf-list given the current buffer
+normi.NormiClear = function()
+	local path = vim.api.nvim_buf_get_name(0)
 	if path == "" then
 		return
 	end
-	local name = vim.fn.fnamemodify(path, ":t:r")
 
+	local name = vim.fn.fnamemodify(path, ":t:r")
 	if name == nil then
 		return
 	end
@@ -101,9 +100,9 @@ M.NormClear = function()
 	qf.clear_errors(plug_id, name)
 end
 
----clear all errors from the qf-list
-M.NormClearAll = function()
+---clear all norminette errors from the qf-list
+normi.NormiClearAll = function()
 	qf.clear_all_errors(plug_id)
 end
 
-return M
+return normi
