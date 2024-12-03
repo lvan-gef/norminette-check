e1a2024fd504542f46e1f6152bc8cc4fac18227f
luuk <lvan-gef@student.codam.nl>
2024-12-03 03:39:51 +0100

    Create function for the default pattern of the plugin in the qf

--- lua/norminette-check/qf_helpers.lua
+++ lua/norminette-check/qf_helpers.lua
@@ -1,5 +1,13 @@
 local qf = {}
 
+--- The format add the end of the line
+---@param name    string The filename without extension
+---@param plug_id string The unique identifier for the plugin
+---@return        string The pattern
+local function get_pattern(name, plug_id)
+    return " [" .. name .. "_" .. plug_id .. "]"
+end
+
 --- Append errors to the quickfix list with a unique plugin identifier.
 --- @param err_list table  A list of error entries to append.
 --- @param plug_id  string The unique identifier for the plugin.
@@ -8,7 +16,7 @@ qf.append_errors = function(err_list, plug_id, name)
     qf.clear_errors(plug_id, name)
 
     for _, entry in ipairs(err_list) do
-        entry.text = entry.text .. " [" .. name .. "_" .. plug_id .. "]"
+        entry.text = entry.text .. get_pattern(name, plug_id)
         vim.fn.setqflist({ entry }, "a")
     end
 end
@@ -21,7 +29,7 @@ qf.clear_errors = function(plug_id, name)
     local new_qflist = {}
 
     for _, entry in ipairs(cur_qflist) do
-        if not string.match(entry.text, name .. "_" .. plug_id .. "]$") then
+        if not string.match(entry.text, get_pattern(name, plug_id)) then
             table.insert(new_qflist, entry)
         end
     end
