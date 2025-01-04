673f49eb1441e9f18878e7fb86c7b9f11d244d0e
luuk <lvan-gef@student.codam.nl>
2025-01-05 00:19:48 +0100

    Removed the argument in the qf_helpers functions, those where redundant

--- lua/norminette-check/qf_helpers.lua
+++ lua/norminette-check/qf_helpers.lua
@@ -1,22 +1,20 @@
 local qf = {}
 
 ---The format add the end of the line
----@param name    string: The filename without extension
 ---@param plug_id string: The unique identifier for the plugin
 ---@return        string: The pattern
-local function get_pattern(name, plug_id)
-  return name .. "_" .. plug_id .. "]"
+local function get_pattern(plug_id)
+  return " [" .. plug_id .. "]"
 end
 
 ---Append errors to the quickfix list with a unique plugin identifier.
----@param name     string: The filename without extension
 ---@param plug_id  string: The unique identifier for the plugin
 ---@param err_list table : List of error entries to append
-qf.set_errors = function(name, plug_id, err_list)
+qf.set_errors = function(plug_id, err_list)
   local new_qflist = {}
 
   for _, entry in ipairs(err_list) do
-    entry.text = entry.text .. " [" .. get_pattern(name, plug_id)
+    entry.text = entry.text .. get_pattern(plug_id)
     table.insert(new_qflist, entry)
   end
 
@@ -24,14 +22,13 @@ qf.set_errors = function(name, plug_id, err_list)
 end
 
 ---Clear errors from the quickfix list given the filename
----@param name    string: The filename without extension
 ---@param plug_id string: The unique identifier for the plugin.
-qf.clear_errors = function(name, plug_id)
+qf.clear_errors = function(plug_id)
   local cur_qflist = vim.fn.getqflist()
   local new_qflist = {}
 
   for _, entry in ipairs(cur_qflist) do
-    if not string.match(entry.text, get_pattern(name, plug_id)) then
+    if not string.match(entry.text, get_pattern(plug_id)) then
       table.insert(new_qflist, entry)
     end
   end
