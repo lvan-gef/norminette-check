790c74f99921f2c3aaf961df88417a227aecc4d3
luuk <lvan-gef@student.codam.nl>
2025-01-05 00:12:54 +0100

    Removed function that make no sense any more

--- lua/norminette-check/qf_helpers.lua
+++ lua/norminette-check/qf_helpers.lua
@@ -39,19 +39,4 @@ qf.clear_errors = function(name, plug_id)
   vim.fn.setqflist(new_qflist, "r")
 end
 
----Clear all errors from the quickfix list
----@param plug_id string: The unique identifier for the plugin.
-qf.clear_all_errors = function(plug_id)
-  local cur_qflist = vim.fn.getqflist()
-  local new_qflist = {}
-
-  for _, entry in ipairs(cur_qflist) do
-    if not string.match(entry.text, plug_id) then
-      table.insert(new_qflist, entry)
-    end
-  end
-
-  vim.fn.setqflist(new_qflist, "r")
-end
-
 return qf
