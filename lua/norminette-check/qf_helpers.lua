c2ecf44bb20520f12776dda7059b18b9294f2f7c
luuk <lvan-gef@student.codam.nl>
2025-01-05 01:42:13 +0100

    Make pattern matching simpler again

--- lua/norminette-check/qf_helpers.lua
+++ lua/norminette-check/qf_helpers.lua
@@ -1,5 +1,5 @@
 local qf = {}
-local plug_id = "[norminette]"
+local plug_id = " [norminette]"
 
 ---Set the errors to the qf-list
 ---@param err_list table : List of error entries to append
@@ -7,7 +7,7 @@ qf.set_errors = function(err_list)
   local new_qflist = {}
 
   for _, entry in ipairs(err_list) do
-    entry.text = entry.text .. " " .. plug_id
+    entry.text = entry.text .. plug_id
     table.insert(new_qflist, entry)
   end
 
@@ -18,12 +18,11 @@ end
 qf.clear_errors = function()
   local cur_qflist = vim.fn.getqflist()
   local new_qflist = {}
-  local suffix = " " .. plug_id
 
   for _, entry in ipairs(cur_qflist) do
     local text_len = #entry.text
-    local suffix_len = #suffix
-    if text_len < suffix_len or entry.text:sub(-suffix_len) ~= suffix then
+    local plug_len = #plug_id
+    if text_len < plug_len or entry.text:sub(-plug_len) ~= plug_id then
       table.insert(new_qflist, entry)
     end
   end
