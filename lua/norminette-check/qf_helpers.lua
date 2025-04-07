d60679b7ad697e64ef2b0814933d8ee6dc10dbdc
Luuk Van Geffen <lvan-gef@f0r6s6.codam.nl>
2025-04-07 17:21:27 +0200

    Bug fix: When there was no norminette error, neovim will give a error because it was not called in a async way

--- lua/norminette-check/qf_helpers.lua
+++ lua/norminette-check/qf_helpers.lua
@@ -1,6 +1,4 @@
 local qf = {}
-local plug_id = " [norminette]"
-local plug_len = #plug_id
 
 ---Set the errors to the qf-list
 ---@param err_list table : List of error entries to append
@@ -8,7 +6,7 @@ qf.set_errors = function(err_list)
   local new_qflist = {}
 
   for _, entry in ipairs(err_list) do
-    entry.text = entry.text .. plug_id
+    entry.text = entry.text
     table.insert(new_qflist, entry)
   end
 
@@ -17,17 +15,9 @@ end
 
 ---Clear errors from the quickfix list given the plugin id
 qf.clear_errors = function()
-  local cur_qflist = vim.fn.getqflist()
-  local new_qflist = {}
-
-  for _, entry in ipairs(cur_qflist) do
-    local text_len = #entry.text
-    if text_len < plug_len or entry.text:sub(-plug_len) ~= plug_id then
-      table.insert(new_qflist, entry)
-    end
-  end
-
-  vim.fn.setqflist(new_qflist, "r")
+  vim.schedule(function ()
+    vim.fn.setqflist({}, "r")
+  end)
 end
 
 return qf
