1c0acd45e96edd103968a52f210af905b6f0b017
Luuk Van Geffen <lvan-gef@f0r6s6.codam.nl>
2025-04-07 17:24:44 +0200

    Format code

--- lua/norminette-check/qf_helpers.lua
+++ lua/norminette-check/qf_helpers.lua
@@ -3,21 +3,21 @@ local qf = {}
 ---Set the errors to the qf-list
 ---@param err_list table : List of error entries to append
 qf.set_errors = function(err_list)
-  vim.schedule(function ()
-  local new_qflist = {}
+  vim.schedule(function()
+    local new_qflist = {}
 
-  for _, entry in ipairs(err_list) do
-    entry.text = entry.text
-    table.insert(new_qflist, entry)
-  end
+    for _, entry in ipairs(err_list) do
+      entry.text = entry.text
+      table.insert(new_qflist, entry)
+    end
 
-  vim.fn.setqflist(new_qflist, "r")
+    vim.fn.setqflist(new_qflist, "r")
   end)
 end
 
 ---Clear errors from the quickfix list given the plugin id
 qf.clear_errors = function()
-  vim.schedule(function ()
+  vim.schedule(function()
     vim.fn.setqflist({}, "r")
   end)
 end
