cde99a326c69ff8263468a13e5919c64289a65e7
Luuk van Geffen <luxesound@MacBook-Pro-van-Luuk.local>
2025-04-09 12:23:43 +0200

    Remove from qf if it was from the plugin

--- lua/norminette-check/qf_helpers.lua
+++ lua/norminette-check/qf_helpers.lua
@@ -1,25 +1,33 @@
 local qf = {}
+local plug_id = " [norminette]"
+local plug_len = #plug_id
 
 ---Set the errors to the qf-list
 ---@param err_list table : List of error entries to append
 qf.set_errors = function(err_list)
-  vim.schedule(function()
-    local new_qflist = {}
+  local new_qflist = {}
 
-    for _, entry in ipairs(err_list) do
-      entry.text = entry.text
-      table.insert(new_qflist, entry)
-    end
+  for _, entry in ipairs(err_list) do
+    entry.text = entry.text .. plug_id
+    table.insert(new_qflist, entry)
+  end
 
-    vim.fn.setqflist(new_qflist, "r")
-  end)
+  vim.fn.setqflist(new_qflist, "r")
 end
 
 ---Clear errors from the quickfix list given the plugin id
 qf.clear_errors = function()
-  vim.schedule(function()
-    vim.fn.setqflist({}, "r")
-  end)
+  local cur_qflist = vim.fn.getqflist()
+  local new_qflist = {}
+
+  for _, entry in ipairs(cur_qflist) do
+    local text_len = #entry.text
+    if text_len < plug_len or entry.text:sub(-plug_len) ~= plug_id then
+      table.insert(new_qflist, entry)
+    end
+  end
+
+  vim.fn.setqflist(new_qflist, "r")
 end
 
 return qf
